677f88ea23f7ee662aaf2946cf7e4eccda82b76a
urdekcah <urdekcah@gmail.com>
2024-11-25 20:28:11 +0900

    feat: add school data retrieval
    Added functionality to `libedbo` for fetching specific school data via
    the `/api/school` endpoint. Unlike `/api/institutions`, which returns an
    array of institutions, this endpoint retrieves detailed information
    about a specific school by its ID.

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ const BASE_URL: &str = "https://registry.edbo.gov.ua";
 const UNIVERSITIES_ENDPOINT: &str = "/api/universities";
 const UNIVERSITY_ENDPOINT: &str = "/api/university";
 const INSTITUTIONS_ENDPOINT: &str = "/api/institutions";
+const SCHOOL_ENDPOINT: &str = "/api/school";
 
 fn assert_some<T>(option: Option<T>, field: &str) -> Result<T, Error> {
   option.ok_or_else(|| Error::OtherError(format!("{} cannot be None", field)))
@@ -78,5 +79,23 @@ pub fn search_institutions(param: SearchParams) -> Result<Vec<Institution>, Erro
   let ut = assert_some(param.institution_category, "institution_category")?;
   let lc = assert_some(param.region, "region")?;
   let url = format!("{BASE_URL}{INSTITUTIONS_ENDPOINT}?ut={ut}&lc={lc}&exp=json");
+  make_request_blocking(url)
+}
+
+pub async fn search_school_async(param: SearchParams) -> Result<Institution, Error> {
+  let id = assert_some(param.id, "id")?;
+  if id < 1 {
+    return Err(Error::OtherError("School ID must be positive".to_string()));
+  }
+  let url = format!("{BASE_URL}{SCHOOL_ENDPOINT}?id={id}&exp=json");
+  make_request(url).await
+}
+
+pub fn search_school(param: SearchParams) -> Result<Institution, Error> {
+  let id = assert_some(param.id, "id")?;
+  if id < 1 {
+    return Err(Error::OtherError("School ID must be positive".to_string()));
+  }
+  let url = format!("{BASE_URL}{SCHOOL_ENDPOINT}?id={id}&exp=json");
   make_request_blocking(url)
 }
